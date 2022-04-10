3d2086c0f372eae1dbe3b983c57025ed8118ade5
allendududu <18133679546@163.com>
2022-04-10 23:51:12 +0800

    TransactionalBusPublishEndpoint

---
+++ src/Coast.Core/EventBus/TransactionEventBus/TransactionalBusPublishEndpoint.cs
@@ -0,0 +1,71 @@
+﻿namespace Coast.Core.EventBus.TransactionBus
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Transactions;
+    using Coast.Core.EventBus.TransactionEventBus;
+    using Microsoft.Extensions.Logging;
+
+    public class TransactionalBusPublishEndpoint : ITransactionalBusPublishEndpoint
+    {
+        readonly ConcurrentDictionary<Transaction, TransactionalEnlistmentNotification> _pendingActions;
+        private readonly IEventBus _bus;
+        private readonly ILogger<TransactionalBusPublishEndpoint> _logger;
+
+        public TransactionalBusPublishEndpoint(IEventBus bus, ILogger<TransactionalBusPublishEndpoint> logger)
+        {
+            _pendingActions = new ConcurrentDictionary<Transaction, TransactionalEnlistmentNotification>();
+            _bus = bus;
+            _logger = logger;
+        }
+
+        /// <inheritdoc/>
+        public void Publish(IntegrationEvent @event, CancellationToken cancellationToken)
+        {
+            Add(() => _bus.Publish(@event, cancellationToken));
+        }
+
+        public async void Add(Action action)
+        {
+            if (Transaction.Current == null)
+            {
+                await Task.Run(() => action());
+                return;
+            }
+
+            var enlistment = GetOrCreateEnlistment();
+
+            enlistment.Add(action);
+
+            return;
+        }
+
+        TransactionalEnlistmentNotification GetOrCreateEnlistment()
+        {
+            return _pendingActions.GetOrAdd(Transaction.Current, transaction =>
+            {
+                var transactionEnlistment = new TransactionalEnlistmentNotification(_logger);
+
+                transaction.TransactionCompleted += TransactionCompleted;
+                transaction.EnlistVolatile(transactionEnlistment, EnlistmentOptions.None);
+
+                return transactionEnlistment;
+            });
+        }
+
+        void ClearTransaction(Transaction transaction)
+        {
+            if (_pendingActions.TryRemove(transaction, out _))
+                transaction.TransactionCompleted -= TransactionCompleted;
+        }
+
+        void TransactionCompleted(object sender, TransactionEventArgs e)
+        {
+            ClearTransaction(e.Transaction);
+        }
+    }
+}
