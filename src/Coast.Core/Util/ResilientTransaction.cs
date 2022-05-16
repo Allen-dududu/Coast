9f15882ff0c3cd044ccf0e085725de3314c81881
allendududu <18133679546@163.com>
2022-05-17 01:04:37 +0800

    remove useless dependency

--- src/Coast.Core/Util/ResilientTransaction.cs
+++ src/Coast.Core/Util/ResilientTransaction.cs
@@ -1,28 +1,28 @@
 ﻿namespace Coast.Core.Util
 {
-    using Microsoft.EntityFrameworkCore;
-    using System;
-    using System.Threading.Tasks;
+    //using Microsoft.EntityFrameworkCore;
+    //using System;
+    //using System.Threading.Tasks;
 
-    public class ResilientTransaction
-    {
-        private DbContext _context;
-        private ResilientTransaction(DbContext context) =>
-            _context = context ?? throw new ArgumentNullException(nameof(context));
+    //public class ResilientTransaction
+    //{
+    //    private DbContext _context;
+    //    private ResilientTransaction(DbContext context) =>
+    //        _context = context ?? throw new ArgumentNullException(nameof(context));
 
-        public static ResilientTransaction New(DbContext context) => new ResilientTransaction(context);
+    //    public static ResilientTransaction New(DbContext context) => new ResilientTransaction(context);
 
-        public async Task ExecuteAsync(Func<Task> action)
-        {
-            //Use of an EF Core resiliency strategy when using multiple DbContexts within an explicit BeginTransaction():
-            //See: https://docs.microsoft.com/en-us/ef/core/miscellaneous/connection-resiliency
-            var strategy = _context.Database.CreateExecutionStrategy();
-            await strategy.ExecuteAsync(async () =>
-            {
-                using var transaction = _context.Database.BeginTransaction();
-                await action();
-                transaction.Commit();
-            });
-        }
-    }
+    //    public async Task ExecuteAsync(Func<Task> action)
+    //    {
+    //        //Use of an EF Core resiliency strategy when using multiple DbContexts within an explicit BeginTransaction():
+    //        //See: https://docs.microsoft.com/en-us/ef/core/miscellaneous/connection-resiliency
+    //        var strategy = _context.Database.CreateExecutionStrategy();
+    //        await strategy.ExecuteAsync(async () =>
+    //        {
+    //            using var transaction = _context.Database.BeginTransaction();
+    //            await action();
+    //            transaction.Commit();
+    //        });
+    //    }
+    //}
 }
